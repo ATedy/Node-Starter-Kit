98855a9d380a8cc07797ae2c4e7605005e3ee2d7
Amanuel <weditedi25@gmail.com>
2021-04-17 12:16:40 +0100

    creating a new server with postman

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-//when server is ready it calls the function
+//when server is ready it calls the callback function
 app.listen(3000, function () {
   console.log("Running on port 3000");
 });
