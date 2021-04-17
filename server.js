ae2bfbc87f61b86c3c03a4e6ee0dbbded57ae600
Amanuel <weditedi25@gmail.com>
2021-04-17 12:07:55 +0100

    server app created

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-const epxress = require("express");
+const express = require("express");
+
+const app = express();
+
+//when server is ready it calls the function
+app.listen(3000, function () {
+  console.log("Running on port 3000");
+});
